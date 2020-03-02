a12f3e55f12abe8db74dfb1d429d90851b438d69
anson <ansonchongwus44@gmail.com>
2020-03-02 15:02:27 -0800

    Create methods for SessionsController

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,25 @@
 class Api::SessionsController < ApplicationController
+    def create
+        @user = User.find_by_credentails(
+            params[:user][:username],
+            params[:user][:password]
+        )
+        if @user 
+            login!(@user)
+            render "api/users/show"
+        else
+            render json: ["Invalid username/password"], status: 401
+        end
+    end 
+
+    def destroy
+        if logged_in?
+            logout()
+            render json: "api/users/user"
+        else
+            render json: ["You are not logged in!"], status: 404
+        end
+    end
+
+
 end
