41d2ab158dffc173c090c6bf18badac810ae4e50
anson <ansonchongwus44@gmail.com>
2020-03-02 14:36:31 -0800

    Create bundle file

--- frontend/hopcamp.jsx
+++ frontend/hopcamp.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// import Root from "./components/root"
+// import configureStore from "./store/store"
 
 document.addEventListener("DOMContentLoaded", ()=>{
+    // const store = configureStore()
     const root = document.getElementById("root")
-
+    // ReactDOM.render(<Root store={store} />, root)
+    ReactDOM.render(<h1>okokoko</h1>, root)
 })
