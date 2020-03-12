b3f1a5c0300230e45b874075d927b7dab80dd535
anson <ansonchongwus44@gmail.com>
2020-03-11 17:39:48 -0700

    Delete weather api

--- frontend/util/weather_api_utils.js
+++
@@ -1,6 +0,0 @@
-// export const fetchTemperatur = ({lat, lng}) => {
-//     $.get({
-//         url: `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${your api key}`
-//     })
-// }
-
