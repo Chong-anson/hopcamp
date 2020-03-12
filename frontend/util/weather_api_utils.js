aefa568d4e421f750044464a16a9ed29d0afeea7
anson <ansonchongwus44@gmail.com>
2020-03-11 17:31:51 -0700

    Fix bug weather api

--- frontend/util/weather_api_utils.js
+++ frontend/util/weather_api_utils.js
@@ -1,6 +1,6 @@
-export const fetchTemperatur = ({lat, lng}) => {
-    $.get({
-        url: `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${your api key}`
-    })
-}
+// export const fetchTemperatur = ({lat, lng}) => {
+//     $.get({
+//         url: `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${your api key}`
+//     })
+// }
 
