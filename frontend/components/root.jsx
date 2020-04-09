e0911d2a2f17ba1545568948c49f537823ca6d4f
anson <ansonchongwus44@gmail.com>
2020-04-09 01:35:03 -0700

    fix scrolling issue for redirect

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
+import ScrollToTop from './scroll_to_top'
 import App from "./app";
 
 const Root = (props) => {
     return(
         <Provider store={props.store}>
             <HashRouter>
+                <ScrollToTop />
                 <App /> 
             </HashRouter>
         </Provider>
