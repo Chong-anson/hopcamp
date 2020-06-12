50afc21a62a96f30b5a3a1a4011df518edf0f7c2
anson <ansonchongwus44@gmail.com>
2020-06-11 22:47:07 -0700

    clean up code

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,22 +1,22 @@
-import React from 'react'; 
+import React from "react";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="logo">
-
-      {/* LOGO IMAGE */}
-      {/* ONE BLOCK */}
+        {/* LOGO IMAGE */}
+        {/* ONE BLOCK */}
       </div>
       <div className="footer-left">
         <div className="footer-left-section">
           <h2 className="footer-title">About me</h2>
           <p>
-            I'm software developer currently living in Seattle. Hopcamp is my 
-            first web application and I'm really proud of it. 
+            I'm software developer currently living in Seattle. Hopcamp is my
+            first web application and I'm really proud of it.
           </p>
-          <p>If you would like to see more of my work, please visit my personal 
-            site at: <a href="https://chong-anson.github.io/portfolio/">here</a>  
+          <p>
+            If you would like to see more of my work, please visit my personal
+            site at: <a href="https://chong-anson.github.io/portfolio/">here</a>
           </p>
           <div className="profile-links">
             <a href="https://github.com/Chong-anson">
@@ -32,18 +32,19 @@ const Footer = () => {
         </div>
       </div>
       <div className="footer-right">
-        <h2 className="footer-title"> About Hopcamp, a pixel-perfect clone of Hipcamp</h2>
-          <p>
-            Hopcamp is built using PostgreSQL, Ruby on Rails, ReactJS and Redux.
-            To enhance user experience, I've incorporated Goolge Maps API, AWS S3, 
-            OpenWeather API.             
-          </p>
+        <h2 className="footer-title">
+          {" "}
+          About Hopcamp, a pixel-perfect clone of Hipcamp
+        </h2>
+        <p>
+          Hopcamp is built using PostgreSQL, Ruby on Rails, ReactJS and Redux.
+        </p>
       </div>
       <div className="footer-bottom">
         <p>&copy; Hopcamp by Anson Chong </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer; 
+export default Footer;
