c4a9273f092b4301e54f91c3108f0013298d3c0c
anson <ansonchongwus44@gmail.com>
2020-06-16 15:21:15 -0700

    style code

--- frontend/components/campsite_show/review_index.jsx
+++ frontend/components/campsite_show/review_index.jsx
@@ -1,88 +1,93 @@
 import React, { useState } from "react";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import ReviewItem from "./review_item";
 import ReviewForm from "./review_form";
 import campsite_detail from "./campsite_detail";
 
-
 const ReviewIndex = (props) => {
   const { campsiteId } = props;
-  const [ button, setButton ] = useState("Add Review");
-  const [ openForm, setForm ] = useState(false);
-  const [ formType, setFormType ] = useState("create")
-  const [ review, setReview ] = useState(null);
+  const [button, setButton] = useState("Add Review");
+  const [openForm, setForm] = useState(false);
+  const [formType, setFormType] = useState("create");
+  const [review, setReview] = useState(null);
 
-  const currentUser = useSelector(state => state.entities.users[state.session.id])
-  
+  const currentUser = useSelector(
+    (state) => state.entities.users[state.session.id]
+  );
+
   const handleReviewAction = (formType, reviewObj = null) => (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     setFormType(formType);
     setReview(reviewObj);
-    
-    if (openForm){
+
+    if (openForm) {
       setForm(false);
       setButton("Add Review");
-    }
-    else {
+    } else {
       setForm(true);
-      setButton("X")
+      setButton("X");
     }
-
   };
 
   const closeForm = () => {
     setForm(false);
     setFormType("create");
     setReview(null);
-    setButton("Add Review")
-  }
+    setButton("Add Review");
+  };
 
   const reviews = useSelector((state) =>
-    state.entities.campsites[campsiteId].reviews.map(id =>
-      state.entities.reviews[id]
-    )).map((review,idx) =>{
-      if (review !== undefined){
-        return <ReviewItem key={`review-${idx}`}review={review} editForm={handleReviewAction("edit", review)} />
-      }
-      else 
-        return null;
-    })
+    state.entities.campsites[campsiteId].reviews.map(
+      (id) => state.entities.reviews[id]
+    )
+  ).map((review, idx) => {
+    if (review !== undefined) {
+      return (
+        <ReviewItem
+          key={`review-${idx}`}
+          review={review}
+          editForm={handleReviewAction("edit", review)}
+        />
+      );
+    } else return null;
+  });
 
-  const addReviewButton = (currentUser === undefined ) ? "" : 
-  (
-    <button
-      className="special-buttons-2"
-      onClick={handleReviewAction("create")}
-    >
-      {button}
-    </button>
-  )
-  
-    return (
-      <div className="show-row">
+  const addReviewButton =
+    currentUser === undefined ? (
+      ""
+    ) : (
+      <button
+        className="special-buttons-2"
+        onClick={handleReviewAction("create")}
+      >
+        {button}
+      </button>
+    );
+
+  return (
+    <div className="show-row">
       <div className="review-title row">
-        <h2> {reviews.length} {reviews.length === 1 ? "review" : "reviews"} </h2>
+        <h2>
+          {" "}
+          {reviews.length} {reviews.length === 1 ? "review" : "reviews"}{" "}
+        </h2>
         {addReviewButton}
       </div>
-      { openForm ? 
-        <ReviewForm 
-        currentUser={currentUser} 
-        campsiteId={campsiteId} 
-        closeForm={closeForm} 
-        type={formType}
-        review={review}
-        /> : 
+      {openForm ? (
+        <ReviewForm
+          currentUser={currentUser}
+          campsiteId={campsiteId}
+          closeForm={closeForm}
+          type={formType}
+          review={review}
+        />
+      ) : (
         ""
-      }
-      <div className="review-container">
-        {reviews}
-      </div>
+      )}
+      <div className="review-container">{reviews}</div>
     </div>
-  
-    )
-
-
+  );
 };
 
-export default ReviewIndex; 
+export default ReviewIndex;
--- frontend/components/campsite_show/review_item.jsx
+++ frontend/components/campsite_show/review_item.jsx
@@ -1,61 +1,62 @@
 import React from "react";
-import { useSelector, useDispatch } from 'react-redux';
-import { deleteReview } from '../../actions/review_actions';
+import { useSelector, useDispatch } from "react-redux";
+import { deleteReview } from "../../actions/review_actions";
 
-const ReviewItem = ({review, editForm}) => {
-  const {reviewer, body} = review;
-  const currentUserId = useSelector(state => state.session.id)
+const ReviewItem = ({ review, editForm }) => {
+  const { reviewer, body } = review;
+  const currentUserId = useSelector((state) => state.session.id);
   const dispatch = useDispatch();
 
-  const icon = (review.recommended) ?
-          <i className="fas fa-thumbs-up"></i> : 
-          <i className="fas fa-thumbs-down"></i>
-  const date = new Date(review.updatedAt)
+  const icon = review.recommended ? (
+    <i className="fas fa-thumbs-up"></i>
+  ) : (
+    <i className="fas fa-thumbs-down"></i>
+  );
+  const date = new Date(review.updatedAt);
 
   // const handleEdit = (e) => {
-  //   e.preventDefault(); 
+  //   e.preventDefault();
   //   editForm("edit", );
   // };
 
   const handleDelete = (e) => {
     e.preventDefault();
     dispatch(deleteReview(review.id));
-  }
+  };
 
-  const reviewButtons = (currentUserId === review.userId) ? (
-    <div className="review-item-buttons">
-      <button className="edit-review"
-        onClick={editForm}
-        >Edit</button>
-      <button className="delete-review"
-        onClick={handleDelete}
-        >Delete</button>
-    </div> ) : ""
+  const reviewButtons =
+    currentUserId === review.userId ? (
+      <div className="review-item-buttons">
+        <button className="edit-review" onClick={editForm}>
+          Edit
+        </button>
+        <button className="delete-review" onClick={handleDelete}>
+          Delete
+        </button>
+      </div>
+    ) : (
+      ""
+    );
 
   return (
     <div className="review-item">
       <div className="review-item-title">
         <div>
-          <div className="recommend-icon">
-            {icon}
-          </div>
-          <p>&nbsp; 
-            <strong> {reviewer} </strong> 
-            {(!review.recommended) ? "does not" : "" } recommends this listing.
+          <div className="recommend-icon">{icon}</div>
+          <p>
+            &nbsp;
+            <strong> {reviewer} </strong>
+            {!review.recommended ? "does not" : ""} recommends this listing.
           </p>
         </div>
-        <p>
-          {date.toString().split(" ").slice(1,4).join(" ")}
-        </p>
+        <p>{date.toString().split(" ").slice(1, 4).join(" ")}</p>
       </div>
       <div className="review-body">
-        <p>
-          {body}
-        </p>
+        <p>{body}</p>
       </div>
-        {reviewButtons}
+      {reviewButtons}
     </div>
-  )
-}
+  );
+};
 
-export default ReviewItem; 
+export default ReviewItem;
