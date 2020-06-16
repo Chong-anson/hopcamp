cc7b0f602aa48281f78c6b580d0512ee47f0e443
anson <ansonchongwus44@gmail.com>
2020-06-16 15:27:14 -0700

    style code

--- frontend/components/splash/splash_item.jsx
+++ frontend/components/splash/splash_item.jsx
@@ -1,31 +1,37 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const SpashItem = (props) => {
-    const {type} = props;
-    const photoUrl = (props[type] && props[type].photoUrl) ? props[type].photoUrl : "#";
-    const handleClick = (e) => {
-      // e.preventDefault();
-      props.updateLocation({location: props[type].name, lat: props[type].lat, lng: props[type].lng})
-    }
-    return(
-        <Link 
-          to={`/search?lat=${props[type].lat}&lng=${props[type].lng}&res=${props[type].name}`} 
-          className="splash-item-link"
-          onClick={handleClick}
-        >
-            <div className="splash-item-background" 
-                style={{ 
-                    backgroundImage: `url(${photoUrl})`,
-                    backgroundPosition: "center"
-                    }}>
-            </div>
-            <div className="splash-item-bottom">
-                <h2>{props[type].description}</h2>
-                <span>{props[type].name}</span>
-            </div>
-        </Link>
-    )
-}
+  const { type } = props;
+  const photoUrl =
+    props[type] && props[type].photoUrl ? props[type].photoUrl : "#";
+  const handleClick = (e) => {
+    // e.preventDefault();
+    props.updateLocation({
+      location: props[type].name,
+      lat: props[type].lat,
+      lng: props[type].lng,
+    });
+  };
+  return (
+    <Link
+      to={`/search?lat=${props[type].lat}&lng=${props[type].lng}&res=${props[type].name}`}
+      className="splash-item-link"
+      onClick={handleClick}
+    >
+      <div
+        className="splash-item-background"
+        style={{
+          backgroundImage: `url(${photoUrl})`,
+          backgroundPosition: "center",
+        }}
+      ></div>
+      <div className="splash-item-bottom">
+        <h2>{props[type].description}</h2>
+        <span>{props[type].name}</span>
+      </div>
+    </Link>
+  );
+};
 
-export default SpashItem; 
+export default SpashItem;
