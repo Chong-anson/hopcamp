ab77f29a842a4f264804ff48cb1cb21f0f77ce58
anson <ansonchongwus44@gmail.com>
2020-06-16 15:22:42 -0700

    style code

--- frontend/components/navbar/navbar_container.jsx
+++ frontend/components/navbar/navbar_container.jsx
@@ -6,21 +6,24 @@ import { openModal, closeModal } from "../../actions/modal_actions";
 import { logout } from "../../actions/session_actions";
 
 const msp = (state, ownProps) => ({
-    currentUser : state.entities.users[state.session.id],
-})
+  currentUser: state.entities.users[state.session.id],
+});
 
 const mdp = (dispatch) => ({
-    logout: () => dispatch(logout()),
-    signupForm: (
-        <button className="navbar-btn"  onClick={() => dispatch(openModal("signup"))} >
-            Sign up
-        </button>
-    ),
-    loginForm: (
-        <button className="navbar-btn" onClick={() => dispatch(openModal("login"))}>
-            Log in
-        </button>
-    )
-})
+  logout: () => dispatch(logout()),
+  signupForm: (
+    <button
+      className="navbar-btn"
+      onClick={() => dispatch(openModal("signup"))}
+    >
+      Sign up
+    </button>
+  ),
+  loginForm: (
+    <button className="navbar-btn" onClick={() => dispatch(openModal("login"))}>
+      Log in
+    </button>
+  ),
+});
 
 export default withRouter(connect(msp, mdp)(Navbar));
