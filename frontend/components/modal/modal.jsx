37463bc1a6ec773dc3c86a722d6e06cbbef7b048
anson <ansonchongwus44@gmail.com>
2020-06-16 15:21:35 -0700

    style code

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -1,54 +1,61 @@
 import React from "react";
 import { connect } from "react-redux";
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import { closeModal } from "../../actions/modal_actions";
-import { SignupFormContainer, LoginFormContainer } from "../session_form/form_container";
+import {
+  SignupFormContainer,
+  LoginFormContainer,
+} from "../session_form/form_container";
 import { clearSessionErrors } from "../../actions/session_actions";
 import { updateFilter } from "../../actions/filter_actions";
-import MoreFilterContainer from '../search/more_filter_container';
+import MoreFilterContainer from "../search/more_filter_container";
 
-function Modal({modal, closeModal, updateFilter}) {
-    if (!modal){
-        return null;
-    }
-    let component; 
-    switch(modal) {
-        case "login": 
-            component = <LoginFormContainer />;
-            break;
-        case "signup":
-            component = <SignupFormContainer />;
-            break;
-        case "filters":
-            component = <MoreFilterContainer updateFilter={updateFilter} closeModal={closeModal} />;
-            break;
-        default:
-            return null;
-    }
-    return (
-        <>
-            <div className="modal-background" onClick={closeModal}>
-            </div>
-            <div className="modal-child" onClick={e => e.stopPropagation()}>
-                { component }
-            </div>
-        </>
-    )
+function Modal({ modal, closeModal, updateFilter }) {
+  if (!modal) {
+    return null;
+  }
+  let component;
+  switch (modal) {
+    case "login":
+      component = <LoginFormContainer />;
+      break;
+    case "signup":
+      component = <SignupFormContainer />;
+      break;
+    case "filters":
+      component = (
+        <MoreFilterContainer
+          updateFilter={updateFilter}
+          closeModal={closeModal}
+        />
+      );
+      break;
+    default:
+      return null;
+  }
+  return (
+    <>
+      <div className="modal-background" onClick={closeModal}></div>
+      <div className="modal-child" onClick={(e) => e.stopPropagation()}>
+        {component}
+      </div>
+    </>
+  );
 }
 const mapStateToProps = (state, ownProps) => {
-    return {
-        modal: state.ui.modal
-    };
+  return {
+    modal: state.ui.modal,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        closeModal: () => {
-            dispatch(clearSessionErrors());
-            return dispatch(closeModal());
-        },
-        updateFilter: (filter, value) => dispatch(updateFilter(filter, value)),
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    closeModal: () => {
+      dispatch(clearSessionErrors());
+      return dispatch(closeModal());
+    },
+    updateFilter: (filter, value) => dispatch(updateFilter(filter, value)),
+  };
 };
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Modal));
