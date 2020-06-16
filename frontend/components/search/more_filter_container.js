b956073b9ed31dc7da1fb9aa26e2450bc6e1bbf7
anson <ansonchongwus44@gmail.com>
2020-06-16 15:23:27 -0700

    style code

--- frontend/components/search/more_filter_container.js
+++ frontend/components/search/more_filter_container.js
@@ -1,53 +1,48 @@
-import { connect } from 'react-redux';
-import MoreFilter from './more_filter';
-import { fetchTags } from '../../actions/tag_actions';
-import { updateFilter, resetTagFilter, updateTagFilter } from '../../actions/filter_actions';
+import { connect } from "react-redux";
+import MoreFilter from "./more_filter";
+import { fetchTags } from "../../actions/tag_actions";
+import {
+  updateFilter,
+  resetTagFilter,
+  updateTagFilter,
+} from "../../actions/filter_actions";
 import { filterCampsites } from "../../reducers/selector";
 
 const msp = (state, ownProps) => {
-    const campsites = filterCampsites(state);
-    const tags = state.entities.tags;
-    const { 
-      checkedTags, 
+  const campsites = filterCampsites(state);
+  const tags = state.entities.tags;
+  const { checkedTags, appliedFilter, minCapacity, maxPrice } = state.ui.filter;
+  if (tags.length) {
+    // categorize tags
+    const categorized = {};
+    tags.forEach((tag) => {
+      if (categorized[tag.category]) categorized[tag.category].push(tag);
+      else categorized[tag.category] = [tag];
+    });
+    return {
+      categorized,
+      campsites,
+      checkedTags,
       appliedFilter,
       minCapacity,
-      maxPrice
-    } = state.ui.filter;
-    if (tags.length){
-    // categorize tags
-        const categorized = {};
-        tags.forEach(tag => {
-            if (categorized[tag.category])
-                categorized[tag.category].push(tag);
-            else
-                categorized[tag.category] = [tag];
-        } )
-        return ({
-            categorized,
-            campsites,
-            checkedTags,
-            appliedFilter,
-            minCapacity,
-            maxPrice
-        })
-    }
-    else{
-      return ({
-        campsites: [], 
-        checkedTags: [], 
-      })
-    }
+      maxPrice,
+    };
+  } else {
+    return {
+      campsites: [],
+      checkedTags: [],
+    };
+  }
 };
 
 const mdp = (dispatch) => ({
   fetchTags: () => dispatch(fetchTags()),
-  updateFilter: (filter, value) => 
-                dispatch(updateFilter(filter, value)),
-  updateTagFilter: ( filter ) => dispatch(updateTagFilter(filter)),
-  resetTagFilter: () => dispatch(resetTagFilter())
-}); 
+  updateFilter: (filter, value) => dispatch(updateFilter(filter, value)),
+  updateTagFilter: (filter) => dispatch(updateTagFilter(filter)),
+  resetTagFilter: () => dispatch(resetTagFilter()),
+});
 
-export default connect(msp,mdp)(MoreFilter);
+export default connect(msp, mdp)(MoreFilter);
 
 // catgorized: {
 //     activities: ["Hiking",
