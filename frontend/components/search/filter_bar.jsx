964c308bd369796c99d3cff9249c277cbc6fcf6d
anson <ansonchongwus44@gmail.com>
2020-06-16 15:23:10 -0700

    style code

--- frontend/components/search/filter_bar.jsx
+++ frontend/components/search/filter_bar.jsx
@@ -2,67 +2,74 @@ import React, { useState, useEffect } from "react";
 import DayPickerInput from "react-day-picker/DayPickerInput";
 
 const FilterBar = (props) => {
-    const CAMPSITE_TYPE = [{Campsites: "CAMPING"}, {RVs: "RV"}, {Lodging: "GLAMPING"}];
-    // const state = { typeFilter: this.CAMPSITE_TYPE};
-    const [typeFilter, setTypeFilter] = useState(CAMPSITE_TYPE); 
-    // props.updateFilter("type", this.CAMPSITE_TYPE);
+  const CAMPSITE_TYPE = [
+    { Campsites: "CAMPING" },
+    { RVs: "RV" },
+    { Lodging: "GLAMPING" },
+  ];
+  // const state = { typeFilter: this.CAMPSITE_TYPE};
+  const [typeFilter, setTypeFilter] = useState(CAMPSITE_TYPE);
+  // props.updateFilter("type", this.CAMPSITE_TYPE);
 
-    const updateTypeFilter = () => {
-        const appliedFilter = [];
-        document.querySelectorAll('button.selected-filter').forEach(el =>
-            appliedFilter.push(el.getAttribute("data-type"))
-        );
-        setTypeFilter(appliedFilter); 
-        // props.updateFilter("type", appliedFilter);
-        props.updateTypeFilter(appliedFilter);
-        // this.setState({ typeFilter }, () => {
-        //     this.props.updateFilter("type", this.state.typeFilter);
-        // });
-    };
+  const updateTypeFilter = () => {
+    const appliedFilter = [];
+    document
+      .querySelectorAll("button.selected-filter")
+      .forEach((el) => appliedFilter.push(el.getAttribute("data-type")));
+    setTypeFilter(appliedFilter);
+    // props.updateFilter("type", appliedFilter);
+    props.updateTypeFilter(appliedFilter);
+    // this.setState({ typeFilter }, () => {
+    //     this.props.updateFilter("type", this.state.typeFilter);
+    // });
+  };
 
-    // const handleButtonClick = () => {
-    //     const el = $(".more-filter-large")
-    //     el.toggleClass("show")
-    // };
- 
-    const handleFilterClick = (e) => {
-        e.preventDefault();
-        $(e.currentTarget).toggleClass("selected-filter");
-        updateTypeFilter();
-    };
+  // const handleButtonClick = () => {
+  //     const el = $(".more-filter-large")
+  //     el.toggleClass("show")
+  // };
 
+  const handleFilterClick = (e) => {
+    e.preventDefault();
+    $(e.currentTarget).toggleClass("selected-filter");
+    updateTypeFilter();
+  };
 
-    const types = CAMPSITE_TYPE.map( (type,idx) => 
-            <button 
-                className="type-filter"
-                onClick={handleFilterClick}
-                data-type={Object.values(type)[0]}
-                key={idx}
-                id={`type-${Object.values(type)[0]}`}
-                >
-                    <span className={`filter-type-icon hc-awesome-${Object.values(type)[0].toLowerCase()}`}></span>
-                    {Object.keys(type)[0]}
-            </button>
-    )
+  const types = CAMPSITE_TYPE.map((type, idx) => (
+    <button
+      className="type-filter"
+      onClick={handleFilterClick}
+      data-type={Object.values(type)[0]}
+      key={idx}
+      id={`type-${Object.values(type)[0]}`}
+    >
+      <span
+        className={`filter-type-icon hc-awesome-${Object.values(
+          type
+        )[0].toLowerCase()}`}
+      ></span>
+      {Object.keys(type)[0]}
+    </button>
+  ));
 
-    // useEffect( () => {props.updateFilter} , [typeFilter]); 
-    useEffect( () => {
-      props.typesFilter.forEach( type => {
-        $(`#type-${type}`).addClass("selected-filter");
-        // updateTypeFilter();
-      })
-    } , []); 
+  // useEffect( () => {props.updateFilter} , [typeFilter]);
+  useEffect(() => {
+    props.typesFilter.forEach((type) => {
+      $(`#type-${type}`).addClass("selected-filter");
+      // updateTypeFilter();
+    });
+  }, []);
 
-    return (
-        <div className="filter-bar">
-            {/* <DayPickerInput />  */}
-            {types}
-            <div className="more-filter">
-                {props.filterButton}
-                {/* <MoreFilterContainer updateFilter={this.props.updateFilter} /> */}
-            </div>
-        </div>
-    )
+  return (
+    <div className="filter-bar">
+      {/* <DayPickerInput />  */}
+      {types}
+      <div className="more-filter">
+        {props.filterButton}
+        {/* <MoreFilterContainer updateFilter={this.props.updateFilter} /> */}
+      </div>
+    </div>
+  );
 };
 
 export default FilterBar;
--- frontend/components/search/more_filter.jsx
+++ frontend/components/search/more_filter.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { filterCampsites, selectAllCategories } from '../../reducers/selector';
+import React, { useState, useEffect } from "react";
+import { filterCampsites, selectAllCategories } from "../../reducers/selector";
 
 class MoreFilter extends React.Component {
   constructor(props) {
@@ -8,8 +8,8 @@ class MoreFilter extends React.Component {
       selectedCampsites: props.campsites,
       appliedFilter: props.appliedFilter,
       checkedTags: props.checkedTags,
-      minCapacity: props.minCapacity, 
-      maxPrice: props.maxPrice
+      minCapacity: props.minCapacity,
+      maxPrice: props.maxPrice,
     };
 
     this.handleSizeChange = this.handleSizeChange.bind(this);
@@ -17,87 +17,85 @@ class MoreFilter extends React.Component {
     this.handleTagChange = this.handleTagChange.bind(this);
     this.clearFilter = this.clearFilter.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  };
-  
+  }
+
   handleSizeChange(e) {
-    const minCapacity = parseInt(e.target.value); 
-    const selectedCampsites = this.state.selectedCampsites.filter( campsite => 
-      campsite.capacity >= minCapacity
+    const minCapacity = parseInt(e.target.value);
+    const selectedCampsites = this.state.selectedCampsites.filter(
+      (campsite) => campsite.capacity >= minCapacity
     );
     this.setState({ selectedCampsites, minCapacity });
-  };
+  }
 
   handlePriceChange(e) {
     const maxPrice = parseInt(e.target.value);
-    const selectedCampsites = this.state.selectedCampsites.filter( campsite => 
-      campsite.price <= maxPrice
-    )
+    const selectedCampsites = this.state.selectedCampsites.filter(
+      (campsite) => campsite.price <= maxPrice
+    );
     this.setState({ selectedCampsites, maxPrice });
-  };
+  }
 
-  handleTagChange(e){
-    // update the filter in the state 
+  handleTagChange(e) {
+    // update the filter in the state
     const checkbox = $(e.target);
     checkbox.toggleClass("selected-more-filter");
     checkbox.parent().toggleClass("checked-label");
     const filters = document.querySelectorAll("input.selected-more-filter");
-    let appliedFilter; 
+    let appliedFilter;
     let { selectedCampsites } = this.state;
-    let checkedTags = []; 
+    let checkedTags = [];
     if (filters.length) {
-        appliedFilter = true;
-        filters.forEach( el => {
-          const newSites = el.getAttribute("data-campsites")
-                              .split(",")
-                              .map( id => parseInt(id))
-          selectedCampsites = selectedCampsites.filter( campsite => 
-            newSites.includes(campsite.id)
-          );
-          checkedTags.push(parseInt(el.id)); 
-        });
+      appliedFilter = true;
+      filters.forEach((el) => {
+        const newSites = el
+          .getAttribute("data-campsites")
+          .split(",")
+          .map((id) => parseInt(id));
+        selectedCampsites = selectedCampsites.filter((campsite) =>
+          newSites.includes(campsite.id)
+        );
+        checkedTags.push(parseInt(el.id));
+      });
     } else {
-        selectedCampsites = this.props.campsites; 
-        appliedFilter = false;
-        checkedTags = [];
-    };        
-    this.setState({ 
-      selectedCampsites, 
-      appliedFilter, 
-      checkedTags
+      selectedCampsites = this.props.campsites;
+      appliedFilter = false;
+      checkedTags = [];
+    }
+    this.setState({
+      selectedCampsites,
+      appliedFilter,
+      checkedTags,
     });
-  };
+  }
 
   handleSubmit(e) {
     e.preventDefault();
-    const {
-      appliedFilter,
-      checkedTags, 
-      minCapacity,
-      maxPrice
-    } = this.state; 
-    const selectedCampsites = this.state.selectedCampsites.map( campsite => campsite.id);
+    const { appliedFilter, checkedTags, minCapacity, maxPrice } = this.state;
+    const selectedCampsites = this.state.selectedCampsites.map(
+      (campsite) => campsite.id
+    );
 
     this.props.updateTagFilter({
-      appliedFilter, 
-      checkedTags, 
-      minCapacity, 
-      maxPrice, 
-      selectedCampsites
+      appliedFilter,
+      checkedTags,
+      minCapacity,
+      maxPrice,
+      selectedCampsites,
     });
     this.props.closeModal();
-  };
+  }
 
   clearFilter(e) {
     e.preventDefault();
-    this.props.resetTagFilter(); 
-    document.querySelectorAll("input.selected-more-filter").forEach( el => {
-      el.checked =  false;
+    this.props.resetTagFilter();
+    document.querySelectorAll("input.selected-more-filter").forEach((el) => {
+      el.checked = false;
       el.classList.remove("selected-more-filter");
-    })
+    });
     const selectedCampsites = [...this.props.campsites];
     this.setState({
       selectedCampsites,
-      checkedTags: []
+      checkedTags: [],
     });
     this.props.closeModal();
   }
@@ -105,74 +103,82 @@ class MoreFilter extends React.Component {
   componentDidMount() {
     if (!this.props.categorized) this.props.fetchTags();
     if (this.state.checkedTags.length) {
-      this.state.checkedTags.forEach( id => {
+      this.state.checkedTags.forEach((id) => {
         const checkbox = $(`#${id}`);
         checkbox.prop("checked", true);
         checkbox.parent().addClass("checked-label");
       });
-    };
-  };
+    }
+  }
 
   componentDidUpdate(prevProps) {
-    if (!prevProps.campsites.length 
-        && this.props.campsites.length) {
-      const selectedCampsites = this.props.campsites; 
-      this.setState({selectedCampsites});
-    };
-  };
+    if (!prevProps.campsites.length && this.props.campsites.length) {
+      const selectedCampsites = this.props.campsites;
+      this.setState({ selectedCampsites });
+    }
+  }
 
   render() {
-    const { minCapacity, maxPrice } = this.state; 
+    const { minCapacity, maxPrice } = this.state;
     const count = this.state.selectedCampsites.length;
 
-    if ( this.props.categorized ) {
+    if (this.props.categorized) {
       const categorizedList = {};
-      
-      Object.keys(this.props.categorized).forEach( key => 
-        categorizedList[key] = Object.values(this.props.categorized[key]).map( tag => {
-          let className = "normal";
-          let icon = null; 
-          if (["Amenities", "Terrain"].includes(tag.category) 
-            || ["Hiking", "Swimming"].includes(tag.name)) {
-            className = "special";
-            icon = <span className={`more-filter-icon 
-                    hc-awesome-${[tag.icon]}`}>
-                    </span>              
-          };
 
-          return (
-            <label 
-              className={`more-filter-label ${className}-label`} 
-              key={tag.id}
-            >
-              <input
-                type="checkbox"
-                className={`${className}-checkbox`}
-                key={`tag-${tag.id}`}
-                name={tag.id}
-                id={tag.id} 
-                onChange={this.handleTagChange}
-                data-campsites={tag.campsites}
-              />
-              {icon}{tag.name}
-            </label>
-          )
-      }));
+      Object.keys(this.props.categorized).forEach(
+        (key) =>
+          (categorizedList[key] = Object.values(
+            this.props.categorized[key]
+          ).map((tag) => {
+            let className = "normal";
+            let icon = null;
+            if (
+              ["Amenities", "Terrain"].includes(tag.category) ||
+              ["Hiking", "Swimming"].includes(tag.name)
+            ) {
+              className = "special";
+              icon = (
+                <span
+                  className={`more-filter-icon 
+                    hc-awesome-${[tag.icon]}`}
+                ></span>
+              );
+            }
+
+            return (
+              <label
+                className={`more-filter-label ${className}-label`}
+                key={tag.id}
+              >
+                <input
+                  type="checkbox"
+                  className={`${className}-checkbox`}
+                  key={`tag-${tag.id}`}
+                  name={tag.id}
+                  id={tag.id}
+                  onChange={this.handleTagChange}
+                  data-campsites={tag.campsites}
+                />
+                {icon}
+                {tag.name}
+              </label>
+            );
+          }))
+      );
       const groupSize = [];
-      for (let i = 1 ; i <= 10; i++) {
+      for (let i = 1; i <= 10; i++) {
         groupSize.push(
           <option key={`size-${i}`} value={i}>
             {i} camper{i > 1 ? "s" : ""}
           </option>
         );
-      };
-      const priceRange = [25, 50, 75, 125, 175].map( (el, idx) => 
-                          (
-                            <option key={`price-${idx}`} value={`${el}`} >
-                              {`Under $${el}`}
-                            </option>
-                          ));
-      const filterSection = Object.keys(categorizedList).map( section => (
+      }
+      const priceRange = [25, 50, 75, 125, 175].map((el, idx) => (
+        <option key={`price-${idx}`} value={`${el}`}>
+          {`Under $${el}`}
+        </option>
+      ));
+      const filterSection = Object.keys(categorizedList).map((section) => (
         <div className="more-filter-section">
           <h2>{section}</h2>
           <div className={`strong-filter ${section.toLowerCase()}`}>
@@ -186,12 +192,12 @@ class MoreFilter extends React.Component {
             <div className="more-filter-section">
               <h2>Group Size</h2>
               <div className="filter-select">
-                <select 
-                  className="form-control" 
+                <select
+                  className="form-control"
                   defaultValue={minCapacity}
                   onChange={this.handleSizeChange}
                 >
-                  <option value="" >Any size</option>
+                  <option value="">Any size</option>
                   {groupSize}
                   <option value="10">10+ campers</option>
                   <option value="15">15+ campers</option>
@@ -200,8 +206,9 @@ class MoreFilter extends React.Component {
               </div>
               <h2>Pricing</h2>
               <div className="filter-select">
-                <select className="form-control"
-                  defaultValue = {maxPrice === 1/0 ? "" : maxPrice}
+                <select
+                  className="form-control"
+                  defaultValue={maxPrice === 1 / 0 ? "" : maxPrice}
                   onChange={this.handlePriceChange}
                 >
                   <option value="">Any price</option>
@@ -213,16 +220,19 @@ class MoreFilter extends React.Component {
             {filterSection}
           </div>
           <div className="filter-buttons">
-            <button className="clear-button" onClick={this.clearFilter} >Clear Filter</button>
-            <button className="special-buttons-2" onClick={this.handleSubmit} > 
-              Show {(count < 10) ? count : `${count}+`} camps 
+            <button className="clear-button" onClick={this.clearFilter}>
+              Clear Filter
+            </button>
+            <button className="special-buttons-2" onClick={this.handleSubmit}>
+              Show {count < 10 ? count : `${count}+`} camps
             </button>
           </div>
         </div>
-    )} else {
-      return null; 
+      );
+    } else {
+      return null;
     }
   }
-};
+}
 
-export default MoreFilter; 
+export default MoreFilter;
