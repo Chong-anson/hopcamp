b2bdad27f575d0b0752acf737ea55d42eda7aa69
anson <ansonchongwus44@gmail.com>
2020-06-16 15:22:16 -0700

    style code

--- frontend/components/navbar/mini_search_container.js
+++ frontend/components/navbar/mini_search_container.js
@@ -1,12 +1,11 @@
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import MiniSearchBox from "./mini_search";
 import { updateLocation } from "../../actions/map_action";
 
-const msp = (state) => ({
-});
+const msp = (state) => ({});
 
 const mdp = (dispatch) => ({
-  updateLocation: (location) => dispatch(updateLocation(location))
-})
+  updateLocation: (location) => dispatch(updateLocation(location)),
+});
 
-export default connect(msp,mdp)(MiniSearchBox);
+export default connect(msp, mdp)(MiniSearchBox);
