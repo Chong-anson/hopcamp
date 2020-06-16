cce4bcf7bdd95e4b231705f86b34b38bb4d75fce
anson <ansonchongwus44@gmail.com>
2020-06-16 15:18:01 -0700

    format code

--- frontend/components/bookings/booking_widget.jsx
+++ frontend/components/bookings/booking_widget.jsx
@@ -1,6 +1,10 @@
-import React, { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux';
-import { createBooking, clearErrors, receiveErrors } from "../../actions/booking_actions";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  createBooking,
+  clearErrors,
+  receiveErrors,
+} from "../../actions/booking_actions";
 import BookingErrors from "../error_show";
 import BookingFormContainer from "./booking_form_container";
 import BookingList from "./booking_list";
@@ -8,11 +12,9 @@ import BookingList from "./booking_list";
 const BookingWidget = (props) => {
   const dispatch = useDispatch();
   const { campsite, currentUser } = props;
-  const [ tab, setTab ] = useState("booking");
+  const [tab, setTab] = useState("booking");
   // BOOKING, CHECKOUT, FINISH
-  const booking = useSelector( state => (
-    state.ui.payment
-  ));
+  const booking = useSelector((state) => state.ui.payment);
   const handleTab = (tab) => {
     setTab(tab);
   };
@@ -21,24 +23,26 @@ const BookingWidget = (props) => {
     e.preventDefault();
     dispatch(createBooking(booking))
       .then(() => setTab("finish"))
-      .fail((err) => dispatch(receiveErrors(err.responseJSON)))
+      .fail((err) => dispatch(receiveErrors(err.responseJSON)));
   };
 
   const handleConfirm = (e) => {
     e.preventDefault();
     dispatch(clearErrors());
     setTab("booking");
-  }
+  };
 
-  let bookingForm = <BookingFormContainer campsite={campsite} setTab={handleTab}/>;
+  let bookingForm = (
+    <BookingFormContainer campsite={campsite} setTab={handleTab} />
+  );
 
-  if (tab === "checkout"){
-    const { startDate, endDate, groupSize } = booking
+  if (tab === "checkout") {
+    const { startDate, endDate, groupSize } = booking;
     const dayDiff = Math.round(endDate - startDate) / (1000 * 24 * 60 * 60);
     bookingForm = (
       <div className="payment-form">
         <div className="payment-title">
-          <h2>Your trip to:  </h2>
+          <h2>Your trip to: </h2>
           <h2>{campsite.name}</h2>
           <div className="space-between">
             <span>Type: </span>
@@ -73,35 +77,31 @@ const BookingWidget = (props) => {
           <h3>${Math.round(dayDiff * campsite.price)}</h3>
         </div>
         <div className="booking-submit-btn">
-          <button className="special-buttons-2"
-            onClick={handleCheckOut}
-          >Check out!</button>
+          <button className="special-buttons-2" onClick={handleCheckOut}>
+            Check out!
+          </button>
         </div>
       </div>
-    )
-  }
-  else if (tab === "finish"){
+    );
+  } else if (tab === "finish") {
     bookingForm = (
       <div className="payment-form">
         <h2>Booking Confirmed. Thank you!</h2>
         <div className="booking-submit-btn">
-          <button className="special-buttons-2"
-            onClick={handleConfirm}
-            >Make another booking!</button>
+          <button className="special-buttons-2" onClick={handleConfirm}>
+            Make another booking!
+          </button>
         </div>
       </div>
-    )
+    );
   }
 
   return (
     <div className="booking-widget">
-    {bookingForm}
-    <BookingList
-      user={currentUser}
-      campsiteId={campsite.id}
-      />
-  </div>
-  )
-}
+      {bookingForm}
+      <BookingList user={currentUser} campsiteId={campsite.id} />
+    </div>
+  );
+};
 
 export default BookingWidget;
